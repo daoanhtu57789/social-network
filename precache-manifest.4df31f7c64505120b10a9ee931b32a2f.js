e584357bf69f8d7863f09314c25e3d45fd6a582f
tudaoanh <tuda62@wru.vn>
2020-04-11 08:07:00 +0700

    Updates

---
+++ precache-manifest.4df31f7c64505120b10a9ee931b32a2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fabc43a7191ca743aae06e9a93d5a63",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "b3ad706928fb281a099d",
+    "url": "/social-network/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "1e33f2487d37e8134955",
+    "url": "/social-network/static/js/2.eead61cf.chunk.js"
+  },
+  {
+    "revision": "58b39d2e087df2618528b7ec2132c7f3",
+    "url": "/social-network/static/js/2.eead61cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3ad706928fb281a099d",
+    "url": "/social-network/static/js/main.42db6649.chunk.js"
+  },
+  {
+    "revision": "08c8d3d8a99eb3191bad",
+    "url": "/social-network/static/js/runtime-main.5bdbdf7e.js"
+  }
+]);
